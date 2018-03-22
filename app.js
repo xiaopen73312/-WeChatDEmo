f0231f2228325d386ef8c56b1e09251e95c15a9b
xiaopen73312 <jackie.peng@oyorooms.com>
2018-03-22 12:23:32 +0800

    tt

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var http = require('service/http.js')
+const APP_ID = 'wxd9184f3fcc08011b';//输入小程序appid  
+const APP_SECRET = '46a2c909ce1ecee963cc146c11c668e2';//输入小程序app_secret  
+// var OPEN_ID = ''//储存获取到openid  
+// var SESSION_KEY = ''//储存获取到session_key 
+ 
 App({
   globalData: {
     userInfo: null,
@@ -8,16 +13,37 @@ App({
    defaultCounty: '',
    defaultIndate:'',
    defaultOutdate:'',
-   defaultEndd:1
+   defaultEndd:1,
+   ACCESS_TOKEN:'',
+   OPEN_ID:'',
+   SESSION_KEY:''
   },
   
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    wx.setStorageSync('logs', logs),
+      this.getAession_key()
   }, 
-  
+  //获取Aession_key
+  getAession_key:function(){
+    var that = this;
+    wx.request({
+      url: 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential', //仅为示例，并非真实的接口地址
+      data: {
+        appid: 'wxd9184f3fcc08011b',
+        secret: '46a2c909ce1ecee963cc146c11c668e2'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data);
+        that.globalData.ACCESS_TOKEN = res.data.access_token;
+      }
+    })
+  },
   getUserInfo: function (cb) {
     var that = this
     if (this.globalData.userInfo) {
@@ -36,27 +62,59 @@ App({
       })
     }
   },
-  //get locationInfo
-   getLocationInfo: function (cb) {
+  //获取openid
+  getOpenIdTap: function () {
     var that = this;
-    if (this.globalData.locationInfo) {
-      cb(this.globalData.locationInfo)
-    } else {
-      wx.getLocation({
-        type: 'gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
-        success: function (res) {
-          that.globalData.locationInfo = res;
-          cb(that.globalData.locationInfo)
-        },
-        fail: function () {
-          // fail
-        },
-        complete: function () {
-          // complete
-        }
-      })
-    }
+    wx.login({
+      success: function (res) {
+        wx.request({
+          //获取openid接口  
+          url: 'https://api.weixin.qq.com/sns/jscode2session',
+          data: {
+            appid: APP_ID,
+            secret: APP_SECRET,
+            js_code: res.code,
+            grant_type: 'authorization_code'
+          },
+          method: 'GET',
+          success: function (res) {
+            console.log(res.data)
+            that.globalData.OPEN_ID = res.data.openid;//获取到的openid  
+            that.globalData.SESSION_KEY = res.data.session_key;//获取到session_key  
+            //  console.log(OPEN_ID)
+            //  console.log(SESSION_KEY)
+            // that.setData({
+            //   openid: res.data.openid.substr(0, 10) + '********' + res.data.openid.substr(res.data.openid.length - 8, res.data.openid.length),
+            //   session_key: res.data.session_key.substr(0, 8) + '********' + res.data.session_key.substr(res.data.session_key.length - 6, res.data.session_key.length)
+            // })
+          }
+        })
+      }
+    })
   },
+  //get locationInfo
+  //  getLocationInfo: function (cb) {
+  //   var that = this;
+  //   if (this.globalData.locationInfo) {
+  //     cb(this.globalData.locationInfo)
+  //   } else {
+  //     wx.getLocation({
+  //       type: 'gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+  //       success: function (res) {
+  //         that.globalData.locationInfo = res;
+  //         cb(that.globalData.locationInfo)
+  //       },
+  //       fail: function () {
+  //         // fail
+  //       },
+  //       complete: function () {
+  //         // complete
+  //       }
+  //     })
+  //   }
+  // },
+  
+
   func: {
     req: http.req
   }
--- pages/reserve/orderinfo/orderinfo.js
+++ pages/reserve/orderinfo/orderinfo.js
@@ -1,3 +1,4 @@
+const app = getApp();
 Page({
 
   /**
@@ -7,6 +8,8 @@ Page({
     
   },
   subMitorder:function() {
+    console.log("提交：", app.globalData.OPEN_ID);
+    this.setMessage();
     wx.redirectTo({
       url: '../pay/pay',
     })
@@ -22,6 +25,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+   
     wx.setNavigationBarTitle({
       title: '订单填写'
     })
@@ -67,5 +71,72 @@ Page({
    */
   onShareAppMessage: function () {
     
-  }
+  },
+
+  formSubmit:function(e)
+    {
+    console.log('e', e)
+    wx.request({
+      url: `https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=${app.globalData.ACCESS_TOKEN}`,
+     
+      data: 
+      {
+        "touser": app.globalData.OPEN_ID,
+        "template_id": "8bYvxe3G44_abYeld5C50m6AecfQ1cezEAoD-TSGohU",
+        "page": "/pages/index/index",
+        "form_id": e.detail.formId,
+        "data": {
+          "keyword1": {
+            "value": "OYO80001",
+            "color": "#173177"
+          },
+          "keyword2": {
+            "value": "2018年01月05日 12:30",
+            "color": "#173177"
+          },
+          "keyword3": {
+            "value": "500",
+            "color": "#173177"
+          },
+          "keyword4": {
+            "value": "201",
+            "color": "#173177"
+          },
+          "keyword5": {
+            "value": "2018-4-1",
+            "color": "#173177"
+          },
+          "keyword6": {
+            "value": "广州市天河区天河路208号",
+            "color": "#173177"
+          },
+          "keyword7": {
+            "value": "大床",
+            "color": "#173177"
+          },
+          "keyword8": {
+            "value": "jackie",
+            "color": "#173177"
+          },
+          "keyword9": {
+            "value": "888888",
+            "color": "#173177"
+          },
+          "keyword10": {
+            "value": "如有需要请联系前台客服88888",
+            "color": "#173177"
+          }
+        },
+        "emphasis_keyword": "keyword1.DATA"
+      } ,
+      method: 'post',
+      header: { 'Content-Type': 'application/json' },
+      success: function (res) {
+        console.log(res.data)
+      },
+      fail: function (res) {
+        console.log(res.data)
+      }
+    })
+    }
 })
