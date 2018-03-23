fb21e1584010cf6524665c3be870b6cfa5e5cafd
xiaopen73312 <jackie.peng@oyorooms.com>
2018-03-23 16:24:43 +0800

    ss

--- pages/order/order.js
+++ pages/order/order.js
@@ -77,7 +77,7 @@ Page({
         var clientHeight = res.windowHeight,
           clientWidth = res.windowWidth,
           rpxR = 750 / clientWidth;
-        var calc = clientHeight * rpxR - 180;
+        var calc = clientHeight * rpxR;
         console.log(calc)
         that.setData({
           winHeight: calc,
